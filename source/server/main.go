3dc82070d999143ae729d0cfac8e02377fa7a796
mocw <984327242@qq.com>
2023-03-06 01:04:32 +0800

    使用gorm gen模式搭建数据

--- source/server/main.go
+++ source/server/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"server/logic/orm/dal"
 	accountService "server/service/account/account"
 	serviceOrder "server/service/account/order"
 	"server/service/server"
 )
 
+const dsn = "chimall:password@tcp(localhost:3306)/chimall?charset=utf8mb4&parseTime=True&loc=Local"
+
 func main() {
 	g := gin.New()
 	//
+
+	const dsn = "chimall:password@tcp(localhost:3306)/chimall?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn))
+	if err != nil {
+		panic(fmt.Errorf("cannot establish db connection: %w", err))
+	}
+	dal.SetDefault(db)
+
+	acc, err := dal.Account.Where(dal.Account.ID.Eq("a")).First()
+
+	fmt.Println(acc)
 	g.POST("/login", server.Login)
 	g.POST("/register", server.Register)
 	g.POST("/logout", server.Logout)
