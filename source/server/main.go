82d3ff96e73598f308fb03ee9491e5cf9753832c
mocw <984327242@qq.com>
2023-03-08 01:31:37 +0800

    增加生成token的方法

--- source/server/main.go
+++ source/server/main.go
@@ -5,20 +5,21 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"server/component/config"
 	"server/logic/orm/dal"
 	accountService "server/service/account/account"
 	serviceOrder "server/service/account/order"
 	"server/service/server"
 )
 
-const dsn = "chimall:password@tcp(localhost:3306)/chimall?charset=utf8mb4&parseTime=True&loc=Local"
-
 func main() {
 	g := gin.New()
 	//
 
 	const dsn = "chimall:password@tcp(localhost:3306)/chimall?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn))
+	//db, err := gorm.Open(mysql.Open(dsn))
+	//换成配置化  从config文件读取
+	db, err := gorm.Open(mysql.Open(config.Config.GetString("mysql.dsn")))
 	if err != nil {
 		panic(fmt.Errorf("cannot establish db connection: %w", err))
 	}
